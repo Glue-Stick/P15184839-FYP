91a199df49968ebf8ae3868f66fb05ff0081b179
Glue-Stick <harry.burroughs1@hotmail.com>
2018-04-09 19:52:35 +0100

    Second Push
    Both SFML and GLEW are setup and linked correctly to the project

--- P15184839 - Harry Burroughs/P15184839 - Harry Burroughs/main.cpp
+++ P15184839 - Harry Burroughs/P15184839 - Harry Burroughs/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+
 
 int main()
 {
+	sf::Window window(sf::VideoMode(800, 600), "Final Year Project");
 
+	while (window.isOpen())
+	{
+		sf::Event e;
+		while (window.pollEvent(e))
+		{
+			if (e.type == sf::Event::Closed)
+				window.close();
+		}
+	}
+
+	return 0;
 }
